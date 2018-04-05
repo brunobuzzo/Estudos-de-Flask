2ef3b5473597302d37af18efa3ec3a49812c2e82
BrunoTomasBuzzo <brunshzl@gmail.com>
2018-04-05 16:38:51 -0300

    Aplicação com o database já em funcionamento

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,28 @@ from flask import Flask
 #importando o Framework
 #importanto a instancia da classe Flask para poder utilizar o Framework em questão
 
-app = Flask(__name__) 
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config.from_object('config') 
 #Instanciando a classe Flask e nomeando a como "app"
 #Essa instancia recebe uma variavel chamada __name__ é uma variavel especial qu eo interpretador Python cria  quando está executando o arquivo e ele sempre atribui um valor que determina qual tipo de arquivo esta executando
 # EX: se é um arquivo principal que está executando ou se é um arquivo secundário
 #se for um arquivo secundário ele fará referencia ao módulo em questão, se for o arquivo principal ele vai atribuir o "main" a variavel.
+
+db= SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 
 
 
 #Para testar, abra o cmd, vá até o diretório que esta salvo seu arquivo e digite: python app.py para rodar o servidor
 #Abra o navegador e digite: localhost:5000 e veja o resultado.
 
-from controllers import default
+from app.controllers import default
